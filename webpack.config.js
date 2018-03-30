277b993f80a937c50425b885977a1f29b39db046
Russell Schmidt <russell.schmidt@me.com>
2018-03-29 17:35:15 -0700

    remove webpack prod files

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,6 @@ module.exports = {
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, "public")
+    contentBase: path.join(__dirname, 'public')
   }
 }
